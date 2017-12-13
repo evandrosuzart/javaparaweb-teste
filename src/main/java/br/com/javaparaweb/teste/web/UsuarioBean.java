bfd5a86581da8e26326d07d319ea88451c18d4c1
evandrosuzart <evandrossuzart@outlook.com>
2017-12-12 23:06:53 -0200

    criado a classe UsuarioBean

---
+++ src/main/java/br/com/javaparaweb/teste/web/UsuarioBean.java
@@ -0,0 +1,91 @@
+package br.com.javaparaweb.teste.web;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class UsuarioBean {
+	private String nome;
+	private String email;
+	private String senha;
+	private String confirmaSenha;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getConfirmaSenha() {
+		return confirmaSenha;
+	}
+	public void setConfirmaSenha(String confirmaSenha) {
+		this.confirmaSenha = confirmaSenha;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((confirmaSenha == null) ? 0 : confirmaSenha.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioBean other = (UsuarioBean) obj;
+		if (confirmaSenha == null) {
+			if (other.confirmaSenha != null)
+				return false;
+		} else if (!confirmaSenha.equals(other.confirmaSenha))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+	
+	public String novo() {
+		return "usuario";
+	}
+	
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		if(!this.senha.equalsIgnoreCase(this.confirmaSenha)) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Senha confirmada incorretamente",""));
+			return "usuario";
+		}
+		
+		// Futuro - salva o usuario
+		return "mostrausuario";
+	}
+}
